23420e639af3a3fb683a289e4ab7a6f73dfb6734
21120447 <haonhat2729@gmail.com>
2024-05-20 16:48:21 +0700

    add more movie list slider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,6 +18,30 @@ const Home = () => {
       })
   })
 
+  const { data: upcommingMovies, isLoading: isLoadingUpcommingMovies } = useQuery({
+    queryKey: ['movies', MovieType.upcoming],
+    queryFn: () =>
+      movieApi.getMovies({
+        movie_type: MovieType.upcoming
+      })
+  })
+
+  const { data: popularMovies, isLoading: isLoadingPopularMovies } = useQuery({
+    queryKey: ['movies', MovieType.popular],
+    queryFn: () =>
+      movieApi.getMovies({
+        movie_type: MovieType.popular
+      })
+  })
+
+  const { data: topRatedMovies, isLoading: isLoadingTopRatedMovies } = useQuery({
+    queryKey: ['movies', MovieType.top_rated],
+    queryFn: () =>
+      movieApi.getMovies({
+        movie_type: MovieType.top_rated
+      })
+  })
+
   const onMovieClicked = ({ id, name }: { id: string; name: string }) => {
     const nameId = generateNameId({
       id,
@@ -39,6 +63,33 @@ const Home = () => {
         title='Now Playing'
         movies={nowPlayingMovies?.data.results}
         isLoading={isLoadingNowPlayingMovies}
+        onMovieClicked={onMovieClicked}
+      />
+    </section>
+
+    <section className='page-container px-3 sm:px-5 mt-10'>
+      <MovieList
+        title='Upcomming'
+        movies={upcommingMovies?.data.results}
+        isLoading={isLoadingUpcommingMovies}
+        onMovieClicked={onMovieClicked}
+      />
+    </section>
+
+    <section className='page-container px-3 sm:px-5 mt-10'>
+      <MovieList
+        title='Top Rated'
+        movies={topRatedMovies?.data.results}
+        isLoading={isLoadingTopRatedMovies}
+        onMovieClicked={onMovieClicked}
+      />
+    </section>
+
+    <section className='page-container px-3 sm:px-5 my-10'>
+      <MovieList
+        title='Popular'
+        movies={popularMovies?.data.results}
+        isLoading={isLoadingPopularMovies}
         onMovieClicked={onMovieClicked}
       />
     </section>
